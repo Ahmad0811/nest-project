665c105c7ee2820bc76447e44e958c824dd880b5
Ahmad <arasahmad000@gmail.com>
2021-09-03 18:05:52 +0300

    initial setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
+import { LikeModule } from './like/like.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [,MongooseModule.forRoot(''),UserModule, PostModule, LikeModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
